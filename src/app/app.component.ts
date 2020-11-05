398066288dd48379c53d3978ea9d4f9d2f0e1c10
Jessele Del Mundo <jessele@hungrynow.co.th>
2020-11-06 03:13:48 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Mini Apps';
+  disclaimer = 'Do not worry! Amazing apps will be uploaded here in a moment';
 }
